4c0331e6d070715795cdf03a4c3118f65ec28dcf
Ashutosh Singh <ashutosh.singh32@infosys.com>
2023-06-18 14:47:23 +0530

    Added route for getUser

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/user');
 const {login, signup} = require('../controllers/auth');
 const {auth, isAdmin, isStudent} = require('../middlewares/auth');
 router.post("/login",login);
@@ -25,6 +25,28 @@ router.get("/admin",auth, isAdmin, (req, res) => {
         success:true,
         message:"Welcome to the Protected route for Admin",
     })
+})
+
+router.get('/getUser', auth, async (req, res) => {
+    try{
+        const id = req.user.id;
+        const user = await User.findById(id);
+        res.status(200).json({
+            success:true,
+            user:user,
+            id:id,
+            message:"Welcome to the Protected User route"
+        })
+    }
+    catch(error){
+        console.log(error);
+            return res.status(500).json({
+                success: false,
+                error:error.message,
+                message: "Failed to fetch User",
+            });
+
+    }   
 })
 
 module.exports = router;
